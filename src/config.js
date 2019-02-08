f07120bc7fc32131f430f503c12fcd6c1471679e
Calin <the8thnoteofficial@gmail.com>
2019-02-08 15:23:59 +0000

    missing a forward slash

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 const config = {
-    api: "http://nintendo64list.uksouth.cloudapp.azure.com:8080/SoloProjectCalin/api"
-    // api: "http://localhost:8080/SoloProjectCalin/api/"
+    api: "http://nintendo64list.uksouth.cloudapp.azure.com:8080/SoloProjectCalin/api/"
+    //api: "http://localhost:8080/SoloProjectCalin/api/"
 }
 
 module.exports = config
